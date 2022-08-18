156e4d0c513c7a28d369f962258981955cdf8c92
RaffeuL <rafael91.lisboa@hotmail.com>
2022-08-18 10:34:24 -0300

    Buscando repositórios

--- src/servicos/requisicoes/usuarios.js
+++ src/servicos/requisicoes/usuarios.js
@@ -1,9 +1,9 @@
 import api from "../api";
-//Iniciar o Servidor: json-server --watch --host 192.168.0.2 db.json;
+//Iniciar o Servidor: json-server --watch --host 192.168.0.2 db.json
 
 export async function buscaUsuario(nomeUsuario) {
     try {
-        const resultado = await api.get(`users?login=${nomeUsuario}`);
+        const resultado = await api.get(`/users?login=${nomeUsuario}`);
         return resultado.data[0];
     } catch (error) {
         console.log(error);
