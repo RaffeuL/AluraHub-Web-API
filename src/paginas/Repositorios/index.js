4a95e10d02a60418df0297237c36ca816f7f1ccc
RaffeuL <rafael91.lisboa@hotmail.com>
2022-08-18 12:06:59 -0300

    Desafio: busca por repositórios parte 1

--- src/paginas/Repositorios/index.js
+++ src/paginas/Repositorios/index.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { Text, View, FlatList, TouchableOpacity, TextInput } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 
-import { pegarRepositoriosDoUsuario } from '../../servicos/requisicoes/repositorios';
+import { pegarRepositoriosDoUsuario, buscarRepositorio } from '../../servicos/requisicoes/repositorios';
 
 import estilos from './estilos';
 
 export default function Repositorios({ route, navigation }) {
     const [repo, setRepo] = useState([]);
+    const [nomeRepositorio, setNomeRepositorio] = useState('')
     const estaNaTela = useIsFocused()
 
     useEffect( async () => {
@@ -15,30 +16,48 @@ export default function Repositorios({ route, navigation }) {
         setRepo(resultado)
     }, [estaNaTela])
 
+    async function buscaRepositorio() {
+        const resultado = await buscarRepositorio(nomeRepositorio)
+    }
+
     return (
         <View style={estilos.container}>
-                <Text style={estilos.repositoriosTexto}>{repo.length} repositórios criados</Text>
-                <TouchableOpacity 
-                    style={estilos.botao}
-                    onPress={() => navigation.navigate('CriarRepositorio')}
-                >
-                    <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
-                </TouchableOpacity>
-        <FlatList
-        data={repo}
-        style={{width: '100%'}}
-        keyExtractor={repo => repo.id}
-        renderItem={({item}) => (
+            <Text style={estilos.repositoriosTexto}>{repo.length} repositórios criados</Text>
             <TouchableOpacity 
-            style={estilos.repositorio}
-            onPress={() => navigation.navigate('InfoRepositorio', {item})}
+                style={estilos.botao}
+                onPress={() => navigation.navigate('CriarRepositorio')}
             >
-                <Text style={estilos.repositorioNome}>{item.name}</Text>
-                <Text style={estilos.repositorioData}>Atualizado em {item.data}</Text>
+                <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
             </TouchableOpacity>
-        )}
-        />
+            <TextInput
+                placeholder="Busque por um repositório"
+                autoCapitalize="none"
+                style={estilos.entrada}
+                value={nomeRepositorio}
+                onChangeText={setNomeRepositorio}
+            />
 
+            <TouchableOpacity style={estilos.botao}
+                onPress={buscaRepositorio}
+            >
+                <Text style={estilos.textoBotao}>
+                    Buscar Repositório
+                </Text>
+            </TouchableOpacity>
+            <FlatList
+            data={repo}
+            style={{width: '100%'}}
+            keyExtractor={repo => repo.id}
+            renderItem={({item}) => (
+                <TouchableOpacity 
+                style={estilos.repositorio}
+                onPress={() => navigation.navigate('InfoRepositorio', {item})}
+                >
+                    <Text style={estilos.repositorioNome}>{item.name}</Text>
+                    <Text style={estilos.repositorioData}>Atualizado em {item.data}</Text>
+                </TouchableOpacity>
+            )}
+            />
         </View>
 
     );
--- src/servicos/requisicoes/repositorios.js
+++ src/servicos/requisicoes/repositorios.js
@@ -2,7 +2,7 @@ import api from "../api";
 
 export async function pegarRepositoriosDoUsuario(id) {
     try {
-        const resultado = await api.get(`repos?postId=${id}`);
+        const resultado = await api.get(`/repos?postId=${id}`);
         return resultado.data;
     } catch (error) {
         console.log(error);
@@ -12,15 +12,24 @@ export async function pegarRepositoriosDoUsuario(id) {
 
 export async function salvarRepositoriosDoUsuario(postId, nome, data, id) {
     try {
-        await api.put(`repos/${id}`,{
+        await api.put(`/repos/${id}`,{
             name: nome,
             data: data,
             postId: postId,
-            id: id
         });
         return 'sucesso'
     } catch (error) {
         console.log(error);
         return 'erro'
+    }
+}
+
+export async function buscarRepositorio(nome) {
+    try{
+        const resultado = await api.get(`/repos?name=${nome}`);
+        return resultado.data[0]
+    }catch(error){
+        console.log(error)
+        return {}
     }
 }
